700f1eed5f90da479745db98759b81e368fa0dab
fops <fopsdev@gmail.com>
2020-03-30 14:09:01 +0200

    sample configs

--- ovl/src/index.ts
+++ ovl/src/index.ts
@@ -1,31 +1,33 @@
 /* uncomment when used from app */
 // replace test with the app
 
-import { overmind, config } from "../../../kaltag/src/index"
 import { IConfig } from "overmind"
+
+import { overmind, config } from "../../../kaltag/src/index"
 import { screens } from "../../../kaltag/src/stateScreens"
 import * as functions from "../../../kaltag/src/functions"
-export type Screen = keyof typeof screens
-export { functions, screens, overmind }
+
+//#################### ovl test prj ######################################
+// import { overmind, config } from "../..//test/src/index"
+// import { screens } from "../../test/src/stateScreens"
+// import * as functions from "../../test/src/functions"
+
+//#################### ovl lib Standalone ######################################
+/* uncomment when used standalone for base dev 
+import { baseOvermindConfig } from "./init"
+import { createOvermind } from "overmind"
+import { screens } from "./state/stateScreens"
+const overmind = createOvermind(baseOvermindConfig, {
+  devtools: true,
+  logProxies: true,
+  delimiter: " "
+})
+*/
+
 declare module "overmind" {
   // tslint:disable:interface-name
   interface Config extends IConfig<typeof config> {}
 }
 
-//###############################################################
-/* uncomment when used standalone for base dev 
-import { baseOvermindConfig } from "./init"
-import { createOvermind, IConfig } from "overmind"
-import { screens, Screen } from "./state/stateScreens"
-export {Screen}
-baseOvermindConfig.state.ovl.screens.screens = screens
-export const overmind = createOvermind(baseOvermindConfig, {
-  devtools: true,
-  logProxies: true,
-  delimiter: " "
-})
-declare module "overmind" {
-  // tslint:disable:interface-name
-  interface Config extends IConfig<typeof baseOvermindConfig> {}
-}
-*/
+export { functions, screens, overmind }
+export type Screen = keyof typeof screens
